e5f8e49795bb4cb93bd7e968ff509e6ad67f33f8
bmcumar <33969329+bmcumar@users.noreply.github.com>
2018-03-22 12:37:08 -0500

    Created the CryptoCurrency class with the getters and setters
    The percentage class has a package at the top

---
+++ CryptoProject/src/CryptoCurrency.java
@@ -0,0 +1,73 @@
+import jdk.jfr.Percentage;
+
+import java.math.BigDecimal;
+import java.time.Period;
+import java.util.UUID;
+
+
+public class CryptoCurrency {
+
+
+    private String tag;
+    private UUID cryptoID;
+    private String name;
+    private BigDecimal currentValue;
+    private BigDecimal amountHeld;
+    private Percentage last4Hours;
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+
+    private UUID getCryptoID() {
+        return cryptoID;// Unique identifier
+    }
+
+    public void setCryptoID(UUID cryptoID) {
+        this.cryptoID = cryptoID;
+    }
+
+    public String getName() {
+        return name;// gets the names of the three CryptoCurrencies
+    }
+
+    public void setName(String name) {
+        this.name = name;// returns the names of the CrytoCurrencies
+    }
+
+    public BigDecimal getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(BigDecimal currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public BigDecimal getAmountHeld() {
+        return amountHeld;
+    }
+
+    public void setAmountHeld(BigDecimal amountHeld) {
+        this.amountHeld = amountHeld;
+    }
+
+    public Percentage getLast4Hours() {
+        return last4Hours;
+    }
+
+    public void setLast4Hours(Percentage last4Hours) {
+        this.last4Hours = last4Hours;
+    }
+
+
+
+
+
+
+
+}
