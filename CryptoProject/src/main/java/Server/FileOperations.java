9616b00c48db5747c060a1786b927cb83264e96c
amee1829 <texasrock17@gmail.com>
2018-04-02 00:44:40 -0500

    Added an append functionality.  I thought it was automatically appending objects but I've tested it in a separate project and it wasn't.  It should append now.  Still tinkering with another concept of it.

--- CryptoProject/src/main/java/Server/FileOperations.java
+++ CryptoProject/src/main/java/Server/FileOperations.java
@@ -25,7 +25,7 @@ public class FileOperations implements java.io.Serializable{
         // created an ObjectOutputStream
 
         try{
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("walletFile.ser"));
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("walletFile.ser", true));
             out.writeObject(wallet);
 
             out.close();
@@ -45,7 +45,7 @@ public class FileOperations implements java.io.Serializable{
 
         try {
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("walletFile.ser"));
-            UUID check = walletToLoad.getWalletID();
+            //UUID check = walletToLoad.getWalletID();
 
             //read in objects and compare til we find specific UUID
             this.saveData = (Wallet) in.readObject();
