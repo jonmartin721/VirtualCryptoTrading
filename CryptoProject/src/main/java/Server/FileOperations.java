a795539a35085e36dc0a66194d6fb2e12e5c6e16
amee1829 <texasrock17@gmail.com>
2018-04-01 20:59:09 -0500

    Populated serialised and deserialised methods. Jon can you take a look at this class? Can you confirm my question on line 65.

--- CryptoProject/src/main/java/Server/FileOperations.java
+++ CryptoProject/src/main/java/Server/FileOperations.java
@@ -8,23 +8,62 @@ be put into one file.
 
 import Client.Wallet;
 
-public class FileOperations {
+import java.io.*;
+
+
+public class FileOperations implements java.io.Serializable{
 
     //TODO -Amee-Serialize the wallet out to a file
-    private Wallet saveData;
+
+    private Wallet saveData;    // variable
 
     //this method will save the wallet passed into it as a serialized object
     public boolean saveWallet(Wallet wallet) {
+        // created a file object from FileOutputStream class to write the object into
+        // created an ObjectOutputStream
+        try{
+            FileOutputStream fileout = new FileOutputStream("walletFile.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileout);
+            out.writeObject(wallet);
 
-        return true;
+            // closing all resources
+            out.close();
+            fileout.close();
+            System.out.println("Serialized data is saved in walletFile.ser");
+            return true;
+        }
+        catch (IOException i){
+            i.printStackTrace();
+            return false;
+        }
 
     }
 
     //TODO -Amee-Deserialize a wallet from file into an object.
     public Wallet loadWallet() {
+        // created a FileInputStream object and set it to the serialized file we want to pull from
+        // created an ObjectInputstream
+        Wallet obj;
+        try {
+            FileInputStream fileIn = new FileInputStream("walletFile.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            obj = (Wallet) in.readObject();
 
-
+            // closing all resources
+            in.close();
+            fileIn.close();
+            return obj;
+        }
+        catch (IOException i){
+            System.out.println("Error: ### Wallet not found ####");
+        }
+        catch (ClassNotFoundException c){
+            System.out.println();
+        }
+        System.out.println();
         return new Wallet("Bob", "Bobby", "Bob123");
+        // **JON-Need to confirm this line above with Jon- so if nothing comes out of the .ser file,
+        // then a new Wallet is created?
 
     }
 
