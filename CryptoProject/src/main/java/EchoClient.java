a1cbe1d3863f9c5b8efc9acd6809511a87fa3f0f
Jon Martin <jonmartin721@gmail.com>
2018-04-21 00:46:49 -0500

    Cleaned up EchoServer class.

--- CryptoProject/src/main/java/EchoClient.java
+++ CryptoProject/src/main/java/EchoClient.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.nio.file.FileSystem;
 
@@ -8,7 +11,6 @@ import java.nio.file.FileSystem;
 public class EchoClient {
 
     private static FileSystem sock;
-
 
     public static void main(String[] args) {
         try {
@@ -32,7 +34,7 @@ public class EchoClient {
             final ObjectInputStream input = new ObjectInputStream(sock.getInputStream());
 
             // loop to send messages
-            Message msg = null, resp = null;
+            Message msg, resp;
             do {
                 //System.out.println("In do-while loop of EchoClient class");
                 // Read and send message.
@@ -59,7 +61,7 @@ public class EchoClient {
 
 
     // Simple method to print a prompt and read a line of text. @return A line of text read from the console
-    static String readSomeText() {
+    private static String readSomeText() {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
             MenuTools.launchScreen();
