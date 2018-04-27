aedb6e7a73a3630baa057aa78fc9efa3877b7963
Jon Martin <jonmartin721@gmail.com>
2018-04-27 00:10:05 -0500

    Removed the free bitcoin, fixed some interface bugs.

--- CryptoProject/src/main/java/Wallet.java
+++ CryptoProject/src/main/java/Wallet.java
@@ -37,7 +37,7 @@ class Wallet implements Serializable {
         totalUsdWithdrawn = new BigDecimal(0);
 
         //add all the cryptos (zeroed out) to the wallet
-        holdings.add(new Cryptocurrency("BTC", new BigDecimal(1)));
+        holdings.add(new Cryptocurrency("BTC", new BigDecimal(0)));
         holdings.add(new Cryptocurrency("ETH", new BigDecimal(0)));
         holdings.add(new Cryptocurrency("XRP", new BigDecimal(0)));
         holdings.add(new Cryptocurrency("BCH", new BigDecimal(0)));
