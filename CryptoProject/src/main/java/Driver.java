30e0b61b43a1eff89edce546a2b8a918e4c5b83b
Jon Martin <jonmartin721@gmail.com>
2019-02-20 21:34:21 -0600

    Changed some settings, removed the dumb server/client functionality I had to have for the assignment.

---
+++ CryptoProject/src/main/java/Driver.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.FileSystem;
+
+
+// Simple client class. This class connects to an EchoServer to send text back and forth.
+// Java message serialization is used to pass Message objects around.
+
+public class Driver {
+
+
+
+    public static void main(String[] args){
+
+        MenuTools.launchScreen();
+    }
+
+
+} //-- end class Driver
